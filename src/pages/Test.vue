15e25f92c411e2dec24eb8e9e8a1e8076d07fde3
Amadeus <fangfengstan@outlook.com>
2022-08-13 14:27:39 +0800

    重构了文件上传组件 22-08-13

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,56 +1,44 @@
 <template>
-  <el-upload
-    :file-list="fileList"
-    class="upload-demo"
-    action="http://localhost:3000/admin/upload"
-    multiple
-    :on-preview="handlePreview"
-    :on-remove="handleRemove"
-    :before-remove="beforeRemove"
-    :limit="3"
-    :on-exceed="handleExceed"
-  >
-    <el-button type="primary">Click to upload</el-button>
+  <el-upload ref="uploadRef" class="upload-demo" action='http://localhost:3000/admin/upload' :auto-upload="false" :headers="headers" :on-success="onSuccess">
+    <template #trigger>
+      <el-button type="primary">select file</el-button>
+    </template>
+
+    <el-button class="ml-3" type="success" @click="submitUpload">
+      upload to server
+    </el-button>
+
     <template #tip>
       <div class="el-upload__tip">
-        jpg/png files with a size less than 500KB.
+        jpg/png files with a size less than 500kb
       </div>
     </template>
   </el-upload>
 </template>
-
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { ElMessage, ElMessageBox } from 'element-plus'
+import { ref, computed } from 'vue'
+import { ElMessage } from 'element-plus'
+import type { UploadInstance } from 'element-plus'
 
-import type { UploadProps, UploadUserFile } from 'element-plus'
+const uploadRef = ref<UploadInstance>()
 
-const fileList = ref<UploadUserFile[]>([
+const headers = computed(() => {
+  return {
+    token: `${localStorage.getItem('token')}`,
+    id: `${localStorage.getItem('_id')}`,
+    username: `${localStorage.getItem('username')}`
+  }
+})
 
-])
-
-const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
-  console.log(file, uploadFiles)
+const onSuccess = (resp,uploadFile,uploadFiles) => {
+  if(resp.status === 1){
+    ElMessage.error(resp.message)
+  }else{
+    ElMessage.success(resp.message)
+  }
 }
 
-const handlePreview: UploadProps['onPreview'] = (uploadFile) => {
-  console.log(uploadFile)
-}
-
-const handleExceed: UploadProps['onExceed'] = (files, uploadFiles) => {
-  ElMessage.warning(
-    `The limit is 3, you selected ${files.length} files this time, add up to ${
-      files.length + uploadFiles.length
-    } totally`
-  )
-}
-
-const beforeRemove: UploadProps['beforeRemove'] = (uploadFile, uploadFiles) => {
-  return ElMessageBox.confirm(
-    `Cancel the transfert of ${uploadFile.name} ?`
-  ).then(
-    () => true,
-    () => false
-  )
+const submitUpload = () => {
+  uploadRef.value!.submit()
 }
 </script>
